b8e6246dc27a11f722f2aecd31bdabe7e62ddcc7
sasayahiromu <sasaya_h@tenso.com>
2018-05-20 02:47:51 +0900

    74

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux'
 import AuthScreen from './src/screens/Auth/Auth'
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace'
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace'
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail'
 import configureStore from './src/store/configureStore'
 
 const store = configureStore();
@@ -12,6 +13,7 @@ const store = configureStore();
 Navigation.registerComponent("awsome-places.AuthScreen", () => AuthScreen, store, Provider);
 Navigation.registerComponent("awsome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("awsome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent("awsome-places.PlaceDetailScreen", () => PlaceDetailScreen);
 
 
 Navigation.startSingleScreenApp({
--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -5,10 +5,22 @@ import {connect} from 'react-redux';
 import PlaceList from '../../components/PlaceList/PlaceList'
 
 class FindPlaceScreen extends Component {
+  ItemSelectedHandler = key =>{
+      const selPlace = this.props.places.find(place => {
+        return place.key === key;
+    })
+      this.props.navigator.push({
+          screen: "awsome-places.PlaceDetailScreen",
+          title:  selPlace.name,
+          passProps: {
+            selectedPlace: selPlace
+          }
+      });
+  }
   render () {
       return (
           <View>
-              <PlaceList places={this.props.places}/>
+              <PlaceList places={this.props.places} onItemSelected={this.ItemSelectedHandler}/>
           </View>
       );
   }
