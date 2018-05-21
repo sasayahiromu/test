871bd85b97f5009b79f0bfff7916eb25280be546
sasayahiromu <sasaya_h@tenso.com>
2018-05-21 12:51:19 +0900

    87

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {View,Text,Button} from 'react-native'
-import startMainTabs from '../MainTabs/startMainTabs'
+import {View,Text,Button, TextInput, StyleSheet} from 'react-native';
+
+import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 
 class AuthScreen extends Component {
 
@@ -10,12 +12,29 @@ class AuthScreen extends Component {
 
   render () {
     return (
-      <View>
-        <Text> Auth Screen </Text>
-        <Button title="Login" onPress={this.loginHandler}/>
+      <View style={styles.container}>
+        <Text> Please Log In </Text>
+        <Button title="Switch to Login"/>
+        <View style={styles.inputContainer}>
+        <DefaultInput placeholder="Your E-Mail Address" />
+        <DefaultInput placeholder="Password" />
+        <DefaultInput placeholder="Confirm Password" />
+        </View>
+        <Button title="Submit" onPress={this.loginHandler}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: "80%"
+  }
+});
 
 export default AuthScreen;
