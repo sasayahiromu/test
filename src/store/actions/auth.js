5c25823196ba9af8fceb6be257d92b738f78370c
sasayahiromu <h.s.298355@gmail.com>
2018-05-28 18:06:17 +0900

    167

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -3,26 +3,47 @@ import firebase from 'react-native-firebase';
 import { uiStartLoading, uiStopLoading } from './index'
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
-export const tryAuth = (authData) => {
+export const tryAuth = (authData, authMode) => {
   return dispatch => {
     dispatch(uiStartLoading());
-    dispatch(authSignup(authData))
+    if (authMode === "login") {
+      dispatch(authSignin(authData))
+    } else {
+      dispatch(authSignup(authData))
+    }
   }
 };
 
-export const authSignup = (authData) => {
+export const authSignup = authData => {
   return dispatch => {
     firebase.auth()
-    .createUserAndRetrieveDataWithEmailAndPassword(authData.email, authData.password)
-    .then(res => {
-      console.log(res);
-      dispatch(uiStopLoading());
-      startMainTabs();
-    })
-    .catch(err => {
-      alert('Authentication failed. Try again.');
-      console.log(err)
-      dispatch(uiStopLoading());
-    })
+      .createUserAndRetrieveDataWithEmailAndPassword(authData.email, authData.password)
+      .then(res => {
+        console.log(res);
+        dispatch(uiStopLoading());
+        startMainTabs();
+      })
+      .catch(err => {
+        alert('Authentication failed. Try again.');
+        console.log(err)
+        dispatch(uiStopLoading());
+      })
   };
+};
+
+export const authSignin = authData => {
+  return dispatch => {
+    firebase.auth()
+      .signInAndRetrieveDataWithEmailAndPassword(authData.email, authData.password)
+      .then(res => {
+        console.log(res);
+        dispatch(uiStopLoading());
+        startMainTabs();
+      })
+      .catch(err => {
+        alert('Authentication failed. Try again.');
+        console.log(err)
+        dispatch(uiStopLoading());
+      })
+  }
 }
